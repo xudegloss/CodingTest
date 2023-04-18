24bec80531d3d8fbd091b52af0422f8118c56e70
xudegloss <ajouunihospitalrn@naver.com>
2023-04-19 02:17:43 +0900

    큐 문제 고민하기

--- python/프로그래머스 코딩테스트 풀이/스택 큐/04프린터(오류).py
+++ python/프로그래머스 코딩테스트 풀이/스택 큐/04프린터(오류).py
@@ -3,15 +3,23 @@ from collections import deque
 def solution(priorities, location):
     # idx : location, val : 값 한꺼번에 표현하기.
     dq=deque((val, idx) for idx, val in enumerate(priorities))
-
-    for _ in range(0, len(dq)):
-        if dq[0][0]<max(dq)[0]:
-            remove_val=dq.popleft()
-            dq.append(remove_val)
+    answer_dq=deque()    
     
-    for i in range(0, len(dq)):
-        if dq[i][1]==location:
-            return i+1
+    iter=0
+    while dq:
+        for _ in range(iter, len(dq)):
+            if dq[iter][0] < max(dq)[0]:
+                cur=dq[iter]
+                dq.remove(cur)
+                dq.append(cur)
+        answer_dq.append(dq[iter])
+        iter+=1
+        if iter==len(dq):
+            break
+    
+    for _ in range(0, len(answer_dq)):
+        if answer_dq[_][1]==location:
+            return _+1
 
 print(solution([2, 1, 3, 2], 2))
 print(solution([1, 1, 9, 1, 1, 1], 0))
