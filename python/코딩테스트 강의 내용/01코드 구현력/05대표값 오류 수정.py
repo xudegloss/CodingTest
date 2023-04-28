11b8c1d31ccd530c620b6300682445b604023623
xudegloss <ajouunihospitalrn@naver.com>
2023-04-28 11:28:43 +0900

    round 함수 추가 설명하기

---
+++ python/코딩테스트 강의 내용/01코드 구현력/05대표값 오류 수정.py
@@ -0,0 +1,16 @@
+## round : round_half_even
+## 근사치가 짝수로 옮겨간다. 
+
+x=4.500
+print(round(x)) # 4
+
+y=4.600
+print(round(y)) # 5
+
+z=3.500
+print(round(z)) # 4
+
+a=66.5
+print(round(a)) # 66
+a=a+0.5
+print(int(a)) # 67
