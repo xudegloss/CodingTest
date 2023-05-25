5e4402cf5d71b4cf9ea0b8ee7acedbc92da03272
xudegloss <ajouunihospitalrn@naver.com>
2023-05-26 00:40:10 +0900

    탐색 2문제 풀이하기

--- python/코딩테스트 강의 내용/02 탐색/07 사과나무.py
+++ python/코딩테스트 강의 내용/02 탐색/07 사과나무.py
@@ -3,3 +3,46 @@ import os
 
 current_path=os.getcwd() 
 sys.stdin=open(current_path+"\\python\\코딩테스트 강의 내용\\02 탐색\\"+"input.txt","rt")
+
+N=int(input())
+res=0
+arr=[]
+
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+
+# 문제 접근 방식 : 나눠서 합하여 전체적으로 더하는 방식으로 풀이할 예정이다.
+# 1. top_res
+# 2. mid_res
+# 3. bottom_res
+# 4. tot_res = top_res + mid_res + bottom_res
+
+# 1. top_res
+
+top_res=arr[0][N//2]
+iter=1
+
+for i in range(1, N//2):
+    for j in range(N//2-iter, (N//2+1)+iter):
+        top_res+=arr[i][j]
+    iter+=1
+
+
+# 2. mid_res
+
+mid_res=sum((arr[N//2]))
+
+# 3. bottom_res
+
+bottom_res=0
+iter=0
+
+for i in range(N-1, N//2, -1):
+    for j in range(N//2-iter, (N//2+1)+iter):
+        bottom_res+=arr[i][j]
+    iter+=1
+    
+# 4. tot_res
+
+tot_res=top_res+mid_res+bottom_res
+print(tot_res)
--- python/코딩테스트 강의 내용/02 탐색/08 곳감.py
+++ python/코딩테스트 강의 내용/02 탐색/08 곳감.py
@@ -3,3 +3,61 @@ import os
 
 current_path=os.getcwd() 
 sys.stdin=open(current_path+"\\python\\코딩테스트 강의 내용\\02 탐색\\"+"input.txt","rt")
+
+N=int(input())
+arr=[]
+
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+
+
+n=int(input())
+
+# 1. 조건에 맞게 회전하기.
+
+## Logic ##
+
+# 왼쪽 0 기준 -> 잘라서 오른쪽에 붙이기.
+# 오른쪽 마지막 인덱스 기준 -> 잘라서 왼쪽에 붙이기.
+
+for iter in range(n):
+    row, dt, move=map(int, input().split())
+
+    if dt==0:
+        arr[row-1] = arr[row-1][move: ] + arr[row-1][ :move]
+    else:
+        arr[row-1] = arr[row-1][ :N-1-move] + arr[row-1][N-1-move: ]
+
+# arr는 회전을 마친 격자판을 의미한다.
+
+# 2. 조건에 맞는 격자에서의 값을 구한 후 합계 구하기.
+# top_res, mid_res, bottom_res
+
+# top_res
+
+iter=1
+top_res=0
+
+for i in range(N//2-1, -1, -1):
+    top_res+=sum(arr[i][N//2-iter : N//2+iter+1])
+    iter+=1
+
+
+# mid_res
+
+mid_res=arr[N//2][N//2]
+
+# bottom_res
+
+iter=1
+bottom_res=0
+
+for i in range(N//2+1, N):
+    bottom_res+=sum(arr[i][N//2-iter : N//2+iter+1])
+    iter+=1
+
+
+# tot_res
+
+tot_res = top_res + mid_res + bottom_res
+print(tot_res)
